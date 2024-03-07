b14eee05cc601456d85c9a0b638951f9966cc3bd
GustavoBraun <gustavo.n.braun@outlook.com>
2024-03-07 16:14:53 -0300

    Iniciando tratamento de erros na função getAnimeById

--- backend/repository/animeRepository.js
+++ backend/repository/animeRepository.js
@@ -1,34 +1,37 @@
 import { db } from "../config/db.js";
 
-export const getAllAnimes = (callback) => {
+export function getAllAnimes (callback) {
     const q = "SELECT * FROM animes"
     db.query(q, (err, data) => {
-        if(err) console.log("Erro: " + err);
+        if(err) console.log("Erro: " + err)
+       return callback(data)
+    })
+}
+
+export const getAnimeById = (id, callback) => {
+    const q = "SELECT * FROM animes WHERE MAL_ID = ?"
+    db.query(q, [id], (err, data) => {
+        if (err) console.log("Erro: " + err);
+        if (data.length == 0) {
+            throw new Error('Anime não encontrado.')
+        }
         return callback(data)
     })
 }
 
-export const getAnimeById = (id) => {
-    const q = "SELECT * FROM animes WHERE MAL_ID = ?"
-    db.query(q, [id], (err, data) => {
-        if (err) console.log("Erro: " + err);
-        return data
-    })
-}
-
-export const getAnimeByName = (name) => {    
+export const getAnimeByName = (name, callback) => {    
     const q = "SELECT * FROM animes WHERE Name = ?"
     db.query(q, [name], (err, data) => {
         if (err) console.log("Erro: " + err);
-        return data
+        return callback(data)
     })
 }
 
-export const getAnimeByGenre = (genre) => {
+export const getAnimeByGenre = (genre, callback) => {
     const q = "SELECT * FROM animes WHERE Genres LIKE '%?%'"
     db.query(q, [genre], (err, data) => {
         if (err) console.log("Erro: " + err);
-        return data
+        return callback(data)
     })
 }
 
@@ -40,18 +43,18 @@ export const deleteAnimeById = (id) => {
     })
 }
 
-export const addNewAnime = (values) => {
+export const addNewAnime = (values, callback) => {
     const q = "INSERT INTO animes (`Name`, `Score`, `Genres`, `sypnopsis`) VALUES (?, ?, ?, ?)"
     db.query(q, [...values], (err, data) => {
         if (err) console.log("Erro: " + err);
-        return data
+        return callback(data)
     })
 }
 
-export const updateAnimeById = (values, id) => {
+export const updateAnimeById = (values, id, callback) => {
     const q = "UPDATE `animesdb`.`animes` SET `Name` = ?, `Score` = ?, `Genres` = ?, `sypnopsis` = ? WHERE `MAL_ID` = ?"
     db.query(q, [...values, id, (err, data) => {
         if (err) console.log("Erro: " + err);
-        return data
+        return callback(data)
     }])
 }
